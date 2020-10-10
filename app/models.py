808448db59906dce62f3c1847b113178b40c590e
Michal Charemza <michal@charemza.name>
2020-10-10 08:35:21 +0100

    refactor: simplify model

--- app/models.py
+++ app/models.py
@@ -2,16 +2,10 @@ import uuid
 from django.db import models
 
 
-class Person(models.Model):
-    pass
-
-
 class Sock(models.Model):
-    updated_at = models.DateTimeField(auto_now=True)
     id_a = models.IntegerField(null=False, unique=True)
     id_b = models.IntegerField(null=False, unique=True)
     hits = models.IntegerField(null=False, default=0)
-    person = models.ForeignKey('Person', null=False, on_delete=models.CASCADE),
     colour = models.CharField(
         max_length=64,
         choices=(('black', 'black'), ('white', 'white')),
