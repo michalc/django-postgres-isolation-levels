07784d9e7e61fb38f30aeef5c661eed732eea40b
Michal Charemza <michal@charemza.name>
2020-10-10 08:36:53 +0100

    feat: show select_for_update can deadlock

---
+++ test_select_for_update_can_deadlock.py
@@ -0,0 +1,65 @@
+##############
+# Setup Django
+
+import django
+django.setup()
+
+
+#############
+# Test proper
+
+import threading
+import random
+import pytest
+from django.db import DatabaseError, transaction
+
+from app.models import Sock
+
+
+@pytest.mark.django_db
+def test_select_for_update_can_deadlock():
+    def create():
+        Sock.objects.all().delete()
+        Sock.objects.create(id_a=1, id_b=1, colour='black')
+        Sock.objects.create(id_a=2, id_b=2, colour='white')
+
+    create_thread = threading.Thread(target=create)
+    create_thread.start()
+    create_thread.join()
+
+    barrier_1 = threading.Barrier(2)
+    barrier_2 = threading.Barrier(2)
+
+    caught = None
+    def update_a():
+        nonlocal caught
+        try:
+            with transaction.atomic():
+                Sock.objects.filter(id_a=1).update(colour='white')
+                barrier_1.wait()
+                barrier_2.wait()
+                Sock.objects.filter(id_a=2).update(colour='black')
+        except Exception as exception:
+            caught = exception
+
+    def update_b():
+        nonlocal caught
+        try:
+            with transaction.atomic():
+                barrier_1.wait()
+                Sock.objects.filter(id_a=2).update(colour='black')
+                barrier_2.wait()
+                Sock.objects.filter(id_a=1).update(colour='white')
+        except Exception as exception:
+            caught = exception
+
+    update_a_thread = threading.Thread(target=update_a)
+    update_a_thread.start()
+    update_b_thread = threading.Thread(target=update_b)
+    update_b_thread.start()
+
+    update_b_thread.join()
+    update_b_thread.join()
+
+    assert isinstance(caught, DatabaseError)
+    assert 'deadlock' in caught.args[0]
