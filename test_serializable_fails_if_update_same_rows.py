1fb916cb64b47224769145973ad72fe4b1f4f263
Michal Charemza <michal@charemza.name>
2020-10-10 09:08:19 +0100

    feat: show serializable fails if update same rows

---
+++ test_serializable_fails_if_update_same_rows.py
@@ -0,0 +1,64 @@
+##############
+# Setup Django
+
+import django
+django.setup()
+
+
+#############
+# Test proper
+
+import threading
+import time
+import pytest
+from django.db import DatabaseError, connection, transaction
+from django.db.models import F, Subquery
+from app.models import Sock
+
+
+@pytest.mark.django_db
+def test_serializable_fails_if_update_same_rows():
+    def create():
+        Sock.objects.all().delete()
+        Sock.objects.create(id_a=1, id_b=1, colour='black')
+
+    create_thread = threading.Thread(target=create)
+    create_thread.start()
+    create_thread.join()
+
+    barrier_1 = threading.Barrier(2)
+    barrier_2 = threading.Barrier(2)
+    barrier_3 = threading.Barrier(2)
+
+    def update_autocommit():
+        sock = Sock.objects.get(id_a=1)
+        barrier_1.wait()
+        barrier_2.wait()
+        sock.save()
+        barrier_3.wait()
+
+    caught = None
+    def update_serializable():
+        nonlocal caught
+        try:
+            with transaction.atomic():
+                cursor = connection.cursor()
+                cursor.execute('SET TRANSACTION ISOLATION LEVEL SERIALIZABLE')
+                barrier_1.wait()
+                sock = Sock.objects.get(id_a=1)
+                barrier_2.wait()
+                barrier_3.wait()
+                sock.save()
+        except Exception as exception:
+            caught = exception
+
+    update_autocommit_thread = threading.Thread(target=update_autocommit)
+    update_autocommit_thread.start()
+    update_serializable_thread = threading.Thread(target=update_serializable)
+    update_serializable_thread.start()
+
+    update_autocommit_thread.join()
+    update_serializable_thread.join()
+
+    assert isinstance(caught, DatabaseError)
+    assert 'could not serialize access due to concurrent update' in caught.args[0]
