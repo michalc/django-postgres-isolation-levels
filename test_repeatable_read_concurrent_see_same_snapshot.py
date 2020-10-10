02ede84de255d23fd4d42d015fa32f8716b9e4e4
Michal Charemza <michal@charemza.name>
2020-10-10 08:57:54 +0100

    refactor: clearer name

--- test_repeatable_read_concurrent_see_same_snapshot.py
+++ test_repeatable_read_concurrent_see_same_snapshot.py
@@ -18,7 +18,7 @@ from app.models import Sock
 
 
 @pytest.mark.django_db
-def test_repeatable_read_multiple_see_same_snapshot():
+def test_repeatable_read_concurrent_see_same_snapshot():
     def create():
         Sock.objects.all().delete()
         Sock.objects.create(id_a=1, id_b=1, colour='black')
